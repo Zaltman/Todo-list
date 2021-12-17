b6d4b0f3c23dc135e07a71fd10e625699f8622f5
Zaltman <mariusdruskinis@gmail.com>
2021-12-17 16:24:52 +0200

    Further todo list render development.

--- src/index.js
+++ src/index.js
@@ -1,57 +1,74 @@
 import './style.css';
 import { domComponentNameClass } from './dom.js';
 import { todoArray } from './todo';
+import { domObj } from './dom.js';
 
 (function pageLoad() {
   document.querySelector('body').appendChild(domComponentNameClass('aside'));
   document.querySelector('body').appendChild(domComponentNameClass('content'));
+})();
+(function sidebarLoad() {
+  document
+    .querySelector('aside')
+    .appendChild(domComponentNameClass('button', 'addBtn')).textContent =
+    '+ Create new task';
 
-  (function sidebarLoad() {
-    document
-      .querySelector('aside')
-      .appendChild(domComponentNameClass('button', 'addBtn')).textContent =
-      '+ Create new task';
+  document
+    .querySelector('aside')
+    .appendChild(domComponentNameClass('button', 'taskList')).textContent =
+    'today';
 
-    document
-      .querySelector('aside')
-      .appendChild(domComponentNameClass('button', 'taskList')).textContent =
-      'today';
+  document
+    .querySelector('aside')
+    .appendChild(domComponentNameClass('button', 'taskList')).textContent =
+    'tomorrow';
 
-    document
-      .querySelector('aside')
-      .appendChild(domComponentNameClass('button', 'taskList')).textContent =
-      'tomorrow';
+  document
+    .querySelector('aside')
+    .appendChild(domComponentNameClass('button', 'taskList')).textContent =
+    'this week';
 
-    document
-      .querySelector('aside')
-      .appendChild(domComponentNameClass('button', 'taskList')).textContent =
-      'this week';
+  document
+    .querySelector('aside')
+    .appendChild(domComponentNameClass('button', 'taskList')).textContent =
+    'next week';
 
-    document
-      .querySelector('aside')
-      .appendChild(domComponentNameClass('button', 'taskList')).textContent =
-      'next week';
+  document
+    .querySelector('aside')
+    .appendChild(domComponentNameClass('button', 'taskList')).textContent =
+    'this month';
 
+  document
+    .querySelector('aside')
+    .appendChild(domComponentNameClass('button', 'taskList')).textContent =
+    'All tasks';
+})();
+(function contentLoad() {
+  document
+    .querySelector('content')
+    .appendChild(domComponentNameClass('ul', 'taskContainer'));
+})();
+(function taskcontainerLoad() {
+  for (let i = 0; i < todoArray.length; i++) {
     document
-      .querySelector('aside')
-      .appendChild(domComponentNameClass('button', 'taskList')).textContent =
-      'this month';
-
+      .querySelector('.taskContainer')
+      .appendChild(
+        domComponentNameClass('li', 'oneTaskContainer')
+      ).dataset.index = i;
     document
-      .querySelector('aside')
-      .appendChild(domComponentNameClass('button', 'taskList')).textContent =
-      'All tasks';
-  })();
-
-  (function contentLoad() {
+      .querySelector('.taskContainer')
+      .lastElementChild.appendChild(
+        domComponentNameClass('div', 'taskN')
+      ).textContent = todoArray[i].title;
     document
-      .querySelector('content')
-      .appendChild(domComponentNameClass('div', 'taskContainer'));
-    (function taskcontainerLoad() {
-      document
-        .querySelector('.taskContainer')
-        .appendChild(domComponentNameClass('div', 'task')).textContent =
-        todoArray[0].title;
-    })();
-  })();
+      .querySelector('.taskContainer')
+      .lastElementChild.appendChild(
+        domComponentNameClass('div', 'taskD')
+      ).textContent = todoArray[i].description;
+    document
+      .querySelector('.taskContainer')
+      .lastElementChild.appendChild(
+        domComponentNameClass('div', 'taskP')
+      ).textContent = todoArray[i].priority;
+  }
 })();
