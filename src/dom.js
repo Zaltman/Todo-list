9350e2e5c8514e9bd5168a16c0b7b647c822e767
Zaltman <mariusdruskinis@gmail.com>
2022-01-04 23:52:25 +0200

    Add filter button functionality

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,5 @@
 import { todoProjects } from '.';
-import { addTodo } from './todo';
+import { addTodo, projectFactory } from './todo';
 function domComponentNameClass(name, className) {
   const element = document.createElement(name);
   if (className) {
@@ -13,13 +13,51 @@ function domProjectsRender(x) {
   let projectTitleEl = document.createElement('button');
   projectTitleEl.textContent = todoProjects[x].title;
   projectTitleEl.classList.add('projectTitle');
-  console.log(projectTitleEl);
+  projectTitleEl.dataset.project = x;
+  projectTitleEl.addEventListener('click', selectiveProjectRender);
+  // console.log(projectTitleEl);
   document.querySelector('.projectContainer').appendChild(projectTitleEl);
 }
 
 function domTodoRender(i, x) {
   let todoTitle = document.createElement('li');
   todoTitle.textContent = todoProjects[x].todoList[i].title;
+  todoTitle.dataset.projectTodo = x;
   document.querySelector('ul').appendChild(todoTitle);
 }
-export { domComponentNameClass, domTodoRender, domProjectsRender };
+
+function selectiveProjectRender(e) {
+  function checkAndDelete(element) {
+    //delete displayed todos and render all todos, then check and delete proper ones
+    function deleteAndRenderAll() {
+      function deleteDomEl(element) {
+        element.remove();
+      }
+      let todoContainer = document.querySelector('.listContainer');
+      let todoNodes = todoContainer.querySelectorAll('li');
+      todoNodes.forEach((element) => deleteDomEl(element));
+    }
+    deleteAndRenderAll();
+    for (let x = 0; x < todoProjects.length; x++) {
+      for (let i = 0; i < todoProjects[x].todoList.length; i++) {
+        domTodoRender(i, x);
+      }
+    }
+    if (element.dataset.projectTodo !== projectIndex) {
+      element.remove();
+    }
+    console.log(element.dataset.projectTodo);
+    console.log(projectIndex);
+  }
+  let projectIndex = e.target.dataset.project;
+  let listContainer = document.querySelector('.listContainer');
+  let todoList = listContainer.querySelectorAll('li');
+  // console.log(todoList);
+  todoList.forEach((element) => checkAndDelete(element, projectIndex));
+}
+export {
+  domComponentNameClass,
+  domTodoRender,
+  domProjectsRender,
+  selectiveProjectRender,
+};
