43c7c1b63594cd2e3ee6fd232e76539409ffa2d0
Zaltman <mariusdruskinis@gmail.com>
2022-01-07 18:06:20 +0200

    Add checkbox functionality.

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,6 @@
 import { el } from 'date-fns/locale';
 import { todoProjects } from '.';
-import { addTodo, projectFactory } from './todo';
+import { addTodo, projectFactory, updateStorage } from './todo';
 function domComponentNameClass(name, className) {
   const element = document.createElement(name);
   if (className) {
@@ -20,28 +20,47 @@ function domProjectsRender(x) {
 }
 
 function domTodoRender(i, x) {
-  //todo title render
-  let todoTitle = document.createElement('li');
-  todoTitle.textContent = todoProjects[x].todoList[i].title;
-  todoTitle.dataset.projectTodo = x;
-  todoTitle.classList.add('todoTitle');
-  document.querySelector('ul').appendChild(todoTitle);
+  //todo container render
+  let todoContainer = document.createElement('li');
+  todoContainer.dataset.project = x;
+  todoContainer.classList.add('todoContainer');
+  document.querySelector('ul').appendChild(todoContainer);
 
-  //todo due date render
-  if (todoProjects[x].todoList[i].dueDate) {
-    let tododueDate = document.createElement('span');
-    tododueDate.textContent = todoProjects[x].todoList[i].dueDate;
-    tododueDate.classList.add('tododueDate');
-    tododueDate.dataset.projectTodo = x;
-    todoTitle.appendChild(tododueDate);
-  }
+  //todo title render
+  let todoTitle = document.createElement('div');
+  todoTitle.textContent = todoProjects[x].todoList[i].title;
+  todoTitle.dataset.project = x;
+  todoTitle.dataset.todo = i;
+  todoContainer.appendChild(todoTitle);
+
   //todo description render
   if (todoProjects[x].todoList[i].description) {
     let todoDescription = document.createElement('li');
     todoDescription.textContent = todoProjects[x].todoList[i].description;
     todoDescription.classList.add('todoDescription');
-    todoDescription.dataset.projectTodo = x;
+    todoDescription.dataset.project = x;
     document.querySelector('ul').appendChild(todoDescription);
+  }
+  // isChecked render
+  let isChecked = document.createElement('input');
+  isChecked.type = 'checkbox';
+  isChecked.value = todoProjects[x].todoList[i].isChecked;
+  isChecked.classList.add('isChecked');
+  isChecked.dataset.project = x;
+  isChecked.dataset.todo = i;
+  isChecked.addEventListener('click', checkUncheck);
+  if (todoProjects[x].todoList[i].isChecked) {
+    isChecked.checked = 'checked';
+  }
+  todoContainer.appendChild(isChecked);
+
+  //todo due date render
+  if (todoProjects[x].todoList[i].dueDate) {
+    let tododueDate = document.createElement('div');
+    tododueDate.textContent = todoProjects[x].todoList[i].dueDate;
+    tododueDate.classList.add('tododueDate');
+    tododueDate.dataset.project = x;
+    todoContainer.appendChild(tododueDate);
   }
 }
 
@@ -49,7 +68,7 @@ function selectiveProjectRender(e) {
   deleteAllDomTodos();
   renderAllDomTodos();
   function checkAndDelete(element) {
-    if (element.dataset.projectTodo !== projectIndex) {
+    if (element.dataset.project !== projectIndex) {
       console.log(element);
       element.remove();
     }
@@ -75,6 +94,18 @@ function renderAllDomTodos() {
       domTodoRender(i, x);
     }
   }
+}
+
+function checkUncheck(e) {
+  let projectIndex = e.target.dataset.project;
+  let todoIndex = e.target.dataset.todo;
+  if (todoProjects[projectIndex].todoList[todoIndex].isChecked) {
+    todoProjects[projectIndex].todoList[todoIndex].isChecked = false;
+    console.log('works');
+  } else {
+    todoProjects[projectIndex].todoList[todoIndex].isChecked = true;
+  }
+  updateStorage();
 }
 
 function renderAllTodosClick() {
